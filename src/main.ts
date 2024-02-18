f46839c41bdd91a0eb768b4f036e91bddd9d3b02
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2024-02-18 14:02:39 -0500

    feat: agregar carpeta assets

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './style.css'
-import logo from '/public/logo.svg'
+import logo from '/public/assets/logo.svg'
+import banner from '/public/assets/banner.jpg'
+import logoFooter from '/public/assets/logo_footer.png'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -9,12 +11,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
         <h2>Producto de Base de Datos II</h2>
     </div>
     
-    <img src="/src/banner.jpg" alt="Banner">
+    <img src="${banner}" alt="Banner">
     <h3>Estadísticas del equipo de fútbol</h3>
     <iframe style="background: #F1F5F4;border: none;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);"  src="https://charts.mongodb.com/charts-estadisticas_futbol-fnugf/embed/dashboards?id=01b03876-6ec2-4859-83e0-eeb8fb88c81e&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed"></iframe>
     
     <footer>
-        <img src="/src/logo_footer.png" alt="Banner">
+        <img src="${logoFooter}" alt="Banner">
         <h2>Club Universitario de Deportes</h2>
         <p>© Desarrollado por <a href="https://pe.linkedin.com/in/ilannestorangelesrodriguez">Ilan Nestor Angeles Rodriguez</a> </p>
     </footer>
